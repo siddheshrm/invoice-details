56d5a359befdfb9e5f589fde0f8da3660b30b771
siddheshrm <siddheshmestri07@gmail.com>
2024-09-26 17:41:37 +0530

    added highlight to selected row, updated indexing

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ fetch("invoice_details.json")
       const row = document.createElement("tr");
       row.innerHTML = `
                 <td>${index === 0 ? '<i class="fa-solid fa-check"></i>' : ""}</td>
-                <td>${item.id}</td>
+                <td>${index + 1}</td>
                 <td>${item.chemical_name || "N/A"}</td>
                 <td>${item.vendor || "N/A"}</td>
                 <td>${item.density ? parseFloat(item.density).toFixed(2) : "N/A"}</td>
@@ -26,6 +26,11 @@ fetch("invoice_details.json")
                 <td>${item.unit || "N/A"}</td>
                 <td>${item.quantity || "N/A"}</td>
             `;
+
+      // Apply 'selected-row' class to the first row when the table loads
+      if (index === 0) {
+        row.classList.add("selected-row");
+      }
 
       // Logging data of the first row for first index
       // if (index === 0) {
@@ -39,11 +44,13 @@ fetch("invoice_details.json")
         // Remove icon from all rows
         const allRows = tableBody.querySelectorAll("tr");
         allRows.forEach((r) => {
+          r.classList.remove("selected-row");
           // Clear the icon cell
           r.cells[0].innerHTML = "";
         });
 
         // Add icon to the clicked row
+        row.classList.add("selected-row");
         row.cells[0].innerHTML = '<i class="fa-solid fa-check"></i>';
       });
 
